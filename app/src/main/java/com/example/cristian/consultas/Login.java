f8c12d85a5469751404ab69e9b2318106b7cb354
Javier Melendez <2513962014@mail.utec.edu.sv>
2018-12-04 21:20:06 -0600

    login
    campos

--- app/src/main/java/com/example/cristian/consultas/Login.java
+++ app/src/main/java/com/example/cristian/consultas/Login.java
@@ -5,45 +5,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.example.cristian.consultas.Enfermeras.Enfermeras;
 import com.example.cristian.consultas.Medicos.Medicos;
 import com.example.cristian.consultas.Pacientes.Paciente;
 
 public class Login extends AppCompatActivity {
-Button ver, btnDoctor,pacientes;
+Button btnIngresar;
+EditText edtDui, edtClave;
+RadioButton rbtPaciente, rbtMedico, rbtEnfermera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ver=findViewById(R.id.btnver);
-        btnDoctor = findViewById(R.id.btnDoctor);
-        pacientes=findViewById(R.id.btnPacientes);
 
+        btnIngresar = findViewById(R.id.btnIngresar);
+        edtDui = findViewById(R.id.edtDui);
+        edtClave = findViewById(R.id.edtClave);
+        rbtEnfermera = findViewById(R.id.rbtEnfermera);
+        rbtMedico = findViewById(R.id.rbtMedico);
+        rbtPaciente = findViewById(R.id.rbtPaciente);
 
-        pacientes.setOnClickListener(new View.OnClickListener() {
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),Paciente.class);
-                startActivity(intent);
+                if(rbtEnfermera.isChecked()){
+                    Intent objEnfermera=new Intent(getApplicationContext(), Enfermeras.class);
+                    startActivity(objEnfermera);
+                }if(rbtMedico.isChecked()){
+                    Intent objMedico=new Intent(getApplicationContext(), Medicos.class);
+                    startActivity(objMedico);
+                }if(rbtPaciente.isChecked()){
+                    Intent objPaciente=new Intent(getApplicationContext(), Paciente.class);
+                    startActivity(objPaciente);
+                }
             }
         });
 
 
-        ver.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),Enfermeras.class);
-                startActivity(intent);
-            }
-        });
 
-        btnDoctor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),Medicos.class);
-                startActivity(intent);
-            }
-        });
+
+
     }
 }
